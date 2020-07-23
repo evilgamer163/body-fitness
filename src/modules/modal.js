512ee517f216d534b0360e44cbb6a3e2e45403ff
evilgamer163 <evilgamestm@gmail.com>
2020-07-23 11:07:18 +0400

    Модальное окно free_visit, добавил git ignore

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,12 +1,40 @@
 'use strict';
 
 const modal = () => {
-    const openPopup = document.querySelector('.open-popup'),
-        freeVisitForm = document.getElementById('free_visit_form');
+    const freeVisitForm = document.getElementById('free_visit_form'),
+        formWrapper = freeVisitForm.querySelector('.form-wrapper');
 
-    openPopup.addEventListener('click', (event) => {
-        event.preventDefault();
+    
+    let count = 0;
+    formWrapper.style.top = '-400px';
+
+    const popupAnim = () => {
+        count++;
         freeVisitForm.style.display = 'block';
+        formWrapper.style.top = count + 'vh';
+        let rafID = requestAnimationFrame(popupAnim);
+        if(count >= 20) {
+            cancelAnimationFrame(rafID);
+        }
+    };
+
+    const closePupop = () => {
+        count = 0;
+        formWrapper.style.top = '-400px';
+        freeVisitForm.style.display = 'none';
+    };
+
+    document.body.addEventListener('click', (event) => {
+        event.preventDefault();
+        let target = event.target,
+            openPopupBtn = target.closest('.open-popup'),
+            formContent= target.closest('.form-content');
+
+        if(openPopupBtn) {
+            popupAnim();
+        } else if(!formContent) {
+            closePupop();
+        }
     });
 };
 

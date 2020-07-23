df354aaaefe542e4186730a509743f6d50439802
evilgamer163 <evilgamestm@gmail.com>
2020-07-23 14:58:28 +0400

    Добавил слайдер в header-main

---
+++ src/modules/slider.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const slider = () => {
+    const mainSlider = document.querySelector('.main-slider'),
+        mainSliderSlides = mainSlider.querySelectorAll('.slide');
+
+    let sliderIndex = 0,
+        animateCount = -400;
+
+    const slideAnimate = () => {
+        animateCount+= 20;
+        mainSliderSlides[sliderIndex].style.left = animateCount + 'px';
+        let rafID = requestAnimationFrame(slideAnimate);
+        if(animateCount >= 0) {
+            animateCount = -400;
+            cancelAnimationFrame(rafID);
+        }
+    };
+
+    const playSlider = () => {
+        mainSliderSlides[sliderIndex].style.display = 'none';
+        sliderIndex++;
+        if(sliderIndex >= mainSliderSlides.length) {
+            sliderIndex = 0;
+        }
+        mainSliderSlides[sliderIndex].style.display = 'flex';
+        slideAnimate();
+    };
+
+    const startSlider = (timer = 1000) => {
+        setInterval(playSlider, timer);
+    };
+
+    startSlider(5000);
+};
+
+export default slider;

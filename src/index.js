dfb79a13863aaaa122aff518c9b491f9fa496377
evilgamer163 <evilgamestm@gmail.com>
2020-07-24 16:11:27 +0400

    Добавил скролл по ссылкам. Добавил кнопку totop

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import modal from './modules/modal';
 import slider from './modules/slider';
 import burgerMenu from './modules/burgerMenu';
 import gallerySlider from './modules/gallerySlider';
+import scroll from './modules/scroll';
 
 selectClub();
 modal();
 slider();
 burgerMenu();
 gallerySlider();
+scroll();
